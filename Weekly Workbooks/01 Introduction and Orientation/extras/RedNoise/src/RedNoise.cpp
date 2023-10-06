917abd8ebac9b0608ca9440831fbf18324e1ab22
SengulC <sengulcagdal@gmail.com>
2023-10-06 15:10:10 +0300

    rainbow 3 elem interp

--- Weekly Workbooks/01 Introduction and Orientation/extras/RedNoise/src/RedNoise.cpp
+++ Weekly Workbooks/01 Introduction and Orientation/extras/RedNoise/src/RedNoise.cpp
@@ -51,15 +51,50 @@ void bAndWdraw(DrawingWindow &window) {
 	}
 }
 
-void draw(DrawingWindow &window) {
+void rainbowDraw(DrawingWindow &window) {
 	window.clearPixels();
-	std::vector<float> colors = interpolateSingleFloats(255.0, 0.0, window.width);
+	glm::vec3 red(255, 0, 0);        // red 
+	glm::vec3 blue(0, 0, 255);       // blue 
+	glm::vec3 green(0, 255, 0);    // green 
+	glm::vec3 yellow(255, 255, 0);   // yellow32_t yellow = (255 << 24) + (int(255) << 16) + (int(255) << 8) + int(0);
+
+	// fill 1st column
+	std::vector<glm::vec3> redyellow = interpolateThreeElementValues(red, yellow, window.height);
+	for (size_t y = 0; y < window.height; y++) {
+		window.setPixelColour(0, y, (255 << 24) + (int(redyellow[y][0]) << 16) + (int(redyellow[y][1]) << 8) + int(redyellow[y][2]));
+	}
+
+	// fill last column
+	std::vector<glm::vec3> bluegreen = interpolateThreeElementValues(blue, green, window.height);
+	for (size_t y = 0; y < window.height; y++) {
+		window.setPixelColour((window.width)-1, y, (255 << 24) + (int(bluegreen[y][0]) << 16) + (int(bluegreen[y][1]) << 8) + int(bluegreen[y][2]));
+	}
+	
+	// fill each row
 	for (size_t y = 0; y < window.height; y++) {
 		for (size_t x = 0; x < window.width; x++) {
-			float red = colors[x];
-			float green = colors[x];
-			float blue = colors[x];
-			uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
+
+			uint32_t from = window.getPixelColour(0, y);
+			uint32_t to = window.getPixelColour(window.width-1, y);
+
+			// XXXXXXXXX XXXXXXXXX XXXXXXXXX XXXXXXXXX
+			int r = (from >> 16) & 0xFF;
+			int g = (from >> 8) & 0xFF;
+			int b = from & 0xFF;
+			glm::vec3 vecFrom(r,g,b);
+
+			r = (to >> 16) & 0xFF;
+			g = (to >> 8) & 0xFF;
+			b = to & 0xFF;
+			glm::vec3 vecTo(r,g,b);
+
+			std::vector<glm::vec3> colors = interpolateThreeElementValues(vecFrom, vecTo, window.width);
+			// have a list of vec3s, need to extract each vec3 to get rgb data
+			glm::vec3 color = colors[x];
+			float redY = color[0];
+			float greenY = color[1];
+			float blueY = color[2];
+			uint32_t colour = (255 << 24) + (int(redY) << 16) + (int(greenY) << 8) + int(blueY);
 			window.setPixelColour(x, y, colour);
 		}
 	}
@@ -83,7 +118,7 @@ int main(int argc, char *argv[]) {
 	while (true) {
 		// We MUST poll for events - otherwise the window will freeze !
 		if (window.pollForInputEvents(event)) handleEvent(event, window);
-		draw(window);
+		rainbowDraw(window);
 		// Need to render the frame at the end, or nothing actually gets shown on the screen !
 		window.renderFrame();
 	}
