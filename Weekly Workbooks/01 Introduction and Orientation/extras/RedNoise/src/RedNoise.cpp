899dd5dd661fdb52e7afbcb048618edf0fea2232
SengulC <sengulcagdal@gmail.com>
2023-11-02 23:24:35 +0000

    manual (removed rasterize from loop)

--- Weekly Workbooks/01 Introduction and Orientation/extras/RedNoise/src/RedNoise.cpp
+++ Weekly Workbooks/01 Introduction and Orientation/extras/RedNoise/src/RedNoise.cpp
@@ -149,13 +149,13 @@ int main(int argc, char *argv[]) {
     std::vector<ModelTriangle> modelTriangles = readObj("models/cornell-box.obj", mtls, 0.35);
 
     // RASTERIZER
-//    twodTriangles = rasterize(window, modelTriangles, cameraPosition, focalLength, scale, depthMatrix);
+    twodTriangles = rasterize(window, modelTriangles, cameraPosition, focalLength, scale, depthMatrix);
     indexcheck = 0;
 
 	while (true) {
 		// We MUST poll for events - otherwise the window will freeze !
 		if (window.pollForInputEvents(event)) handleEvent(event, window);
-        twodTriangles = rasterize(window, modelTriangles, cameraPosition, focalLength, scale, depthMatrix);
+//        twodTriangles = rasterize(window, modelTriangles, cameraPosition, focalLength, scale, depthMatrix);
 		// Need to render the frame at the end, or nothing actually gets shown on the screen !
 		window.renderFrame();
 	}
