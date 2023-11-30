351bc8d25a7e900c5d4ecc0e5d5dae690cc4ecc8
Sengul Cagdal <tx21026@it075841.wks.bris.ac.uk>
2023-11-30 15:51:16 +0000

    got stuff working on labmach

--- Weekly Workbooks/01 Introduction and Orientation/extras/RedNoise/libs/sdw/OurTriangle.h
+++ Weekly Workbooks/01 Introduction and Orientation/extras/RedNoise/libs/sdw/OurTriangle.h
@@ -14,6 +14,7 @@
 #include "OurLine.h"
 #include "TextureMap.h"
 #include "TexturePoint.h"
+#include <algorithm>
 
 std::vector<std::vector<float>> drawStroked(DrawingWindow &window, CanvasTriangle triangle, Colour color, std::vector<std::vector<float>> depthMatrix);
 std::vector<std::vector<float>> drawFilled(DrawingWindow &window, CanvasTriangle triangle, Colour color, std::vector<std::vector<float>> depthMatrix);
