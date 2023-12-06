248b2b3f9fe3412ea6d20b6a081e251a1a25b8a3
Sengul Cagdal <tx21026@it075770.wks.bris.ac.uk>
2023-12-06 19:24:43 +0000

    got lighting ppms and vids

--- Weekly Workbooks/01 Introduction and Orientation/extras/RedNoise/libs/sdw/OurRender.cpp
+++ Weekly Workbooks/01 Introduction and Orientation/extras/RedNoise/libs/sdw/OurRender.cpp
@@ -266,10 +266,10 @@ void drawRaytracedScene(DrawingWindow &window, const std::vector<ModelTriangle>&
                 glm::vec3 surfaceToCam(cameraPosition-intersection.intersectionPoint);
                 float specular = glm::normalizeDot(reflectionVector, surfaceToCam);
 
-                specular = pow(specular, 1024);
+                specular = pow(specular, 512);
 
                 // restrict a given value between 0-1
-                float intensity = /*(brightness*angle*5)+*/specular;
+                float intensity = (brightness*angle*5)+specular;
 //                float intensity = angle;
                 brightnesses.push_back(intensity);
 //                std::cout<<intensity<<std::endl;
@@ -279,20 +279,20 @@ void drawRaytracedScene(DrawingWindow &window, const std::vector<ModelTriangle>&
                     intensity = 0.1;
                 }
 
-//                if (closestObjIntersection.valid &&
-//                    glm::distance(closestObjIntersection.intersectionPoint, intersection.intersectionPoint) >= 0.0001) {
-//                        // SHADOW
-//                        Colour currColor = intersection.intersectedTriangle.colour;
-//                        uint32_t shadow = convertColor(Colour(currColor.red *0.3, currColor.green *0.3, currColor.blue *0.3));
-//                        window.setPixelColour(x, y, shadow);
-//                } else if (intersection.intersectedTriangle.colour.name=="White") {
-//                    // hardcoding lightbox lol
-//                    window.setPixelColour(x, y, convertColor(Colour(255,255,255)));
-//                } else {
+                if (closestObjIntersection.valid &&
+                    glm::distance(closestObjIntersection.intersectionPoint, intersection.intersectionPoint) >= 0.0001) {
+                        // SHADOW
+                        Colour currColor = intersection.intersectedTriangle.colour;
+                        uint32_t shadow = convertColor(Colour(currColor.red *0, currColor.green *0, currColor.blue *0));
+                        window.setPixelColour(x, y, shadow);
+                } /*else if (intersection.intersectedTriangle.colour.name=="White") {
+                    // hardcoding lightbox lol
+                    window.setPixelColour(x, y, convertColor(Colour(255,255,255)));
+                } */else {
                     Colour currColor = intersection.intersectedTriangle.colour;
                     uint32_t color = convertColor(Colour(currColor.red * intensity, currColor.green * intensity, currColor.blue * intensity));
                     window.setPixelColour(x, y, color);
-//                }
+                }
             }
         }
     }
